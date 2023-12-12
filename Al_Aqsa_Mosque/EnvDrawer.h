86282471b40237d98ab7119592476f3f9f9094f8
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-12-12 04:49:31 +0300

    add docs for MosqueDrawer and EnvDrawer

--- Al_Aqsa_Mosque/EnvDrawer.h
+++ Al_Aqsa_Mosque/EnvDrawer.h
@@ -6,27 +6,98 @@
 #include "Color.h"
 #include "Model_3DS.h"  
 
+/**
+ * @class EnvDrawer
+ * @brief A utility class for drawing environmental elements such as trees, tanks, land, and skyboxes.
+ */
 class EnvDrawer {
 public:
-    EnvDrawer();
-    void drawSmallTree(const Point& position, const float size);
-    void drawBigTree(const Point& position, const float size);
-    void drawTank(const Point& position, const float size);
-    void drawTiledLand(const Point points[4],const int count);
-    void drawGrassLand(const Point points[4], const int count);
-    void drawCitySkyBox(const Point& position, const Constraints& constraints);
-    void drawCloudsSkyBox(const Point& position, const Constraints& constraints);
+	/**
+	 * @brief Constructor for the EnvDrawer class.
+	 */
+	EnvDrawer();
+
+	/**
+	 * @brief Draw a small tree at the specified position with the given size.
+	 * @param position The position where the tree will be drawn.
+	 * @param size The size of the tree.
+	 */
+	void drawSmallTree(const Point& position, const float size);
+
+	/**
+	 * @brief Draw a big tree at the specified position with the given size.
+	 * @param position The position where the tree will be drawn.
+	 * @param size The size of the tree.
+	 */
+	void drawBigTree(const Point& position, const float size);
+
+	/**
+	 * @brief Draw a tank at the specified position with the given size.
+	 * @param position The position where the tank will be drawn.
+	 * @param size The size of the tank.
+	 */
+	void drawTank(const Point& position, const float size);
+
+	/**
+	 * @brief Draw a tiled land using the specified points and count.
+	 * @param points An array of points representing the land.
+	 * @param count The number of points in the array.
+	 */
+	void drawTiledLand(const Point points[4], const int count);
+
+	/**
+	 * @brief Draw a grassy land using the specified points and count.
+	 * @param points An array of points representing the land.
+	 * @param count The number of points in the array.
+	 */
+	void drawGrassLand(const Point points[4], const int count);
+
+	/**
+	 * @brief Draw a city skybox at the specified position and constraints.
+	 * @param position The position where the skybox will be drawn.
+	 * @param constraints The constraints defining the size of the skybox.
+	 */
+	void drawCitySkyBox(const Point& position, const Constraints& constraints);
+
+	/**
+	 * @brief Draw a clouds skybox at the specified position and constraints.
+	 * @param position The position where the skybox will be drawn.
+	 * @param constraints The constraints defining the size of the skybox.
+	 */
+	void drawCloudsSkyBox(const Point& position, const Constraints& constraints);
+
 private:
-     static Model_3DS* tree1Model;  
-     static Model_3DS* tree2Model;
-     static Model_3DS* tankModel;
-     int ground, grass, stone;
-     int SKYFRONT, SKYBACK, SKYLEFT, SKYRIGHT, SKYUP, SKYDOWN;
-     int CitySKYFRONT, CitySKYBACK, CitySKYLEFT, CitySKYRIGHT, CitySKYUP, CitySKYDOWN;
-     int CloudsSKYFRONT, CloudsSKYBACK, CloudsSKYLEFT, CloudsSKYRIGHT, CloudsSKYUP, CloudsSKYDOWN;
-     void drawLand(const Point points[4], const int count, const int textureID);
-     void drawModel( Model_3DS* model,const Point& position, const float size, const float scaleFactor);
-     void drawSkyBox(const Point& position,const Constraints& constraints);
+	static Model_3DS* tree1Model;
+	static Model_3DS* tree2Model;
+	static Model_3DS* tankModel;
+	int ground, grass, stone;
+	int SKYFRONT, SKYBACK, SKYLEFT, SKYRIGHT, SKYUP, SKYDOWN;
+	int CitySKYFRONT, CitySKYBACK, CitySKYLEFT, CitySKYRIGHT, CitySKYUP, CitySKYDOWN;
+	int CloudsSKYFRONT, CloudsSKYBACK, CloudsSKYLEFT, CloudsSKYRIGHT, CloudsSKYUP, CloudsSKYDOWN;
+
+	/**
+	 * @brief Draw a model at the specified position with the given size and scale factor.
+	 * @param model The 3D model to be drawn.
+	 * @param position The position where the model will be drawn.
+	 * @param size The size of the model.
+	 * @param scaleFactor The scale factor applied to the model.
+	 */
+	void drawModel(Model_3DS* model, const Point& position, const float size, const float scaleFactor);
+
+	/**
+	 * @brief Draw a land using the specified points, count, and texture ID.
+	 * @param points An array of points representing the land.
+	 * @param count The number of points in the array.
+	 * @param textureID The texture ID for the land.
+	 */
+	void drawLand(const Point points[4], const int count, const int textureID);
+
+	/**
+	 * @brief Draw a skybox at the specified position and constraints.
+	 * @param position The position where the skybox will be drawn.
+	 * @param constraints The constraints defining the size of the skybox.
+	 */
+	void drawSkyBox(const Point& position, const Constraints& constraints);
 };
 
 #endif // ENV_DRAWER_H
--- Al_Aqsa_Mosque/MosqueDrawer.h
+++ Al_Aqsa_Mosque/MosqueDrawer.h
@@ -5,30 +5,76 @@
 #include "Color.h"
 #include "Sphere.h"
 #include "Cylinder.h"
-#include "Model_3DS.h" 
-
+#include "Model_3DS.h"
 #define GOLDEN_DOME Color(1.0f, 0.84f, 0.0f)
 #define SILVER_DOME Color(0.5f, 0.5f, 0.5f)
 #define DOME_SHADOW Color(0.0f, 0.0f, 0.0f)
-
+/**
+ * @class MosqueDrawer
+ * @brief A utility class for drawing mosque-related structures, such as domes and prayer carpets, using OpenGL.
+ */
 class MosqueDrawer {
 public:
-    MosqueDrawer();
     /**
-     * Draws a customizable dome using OpenGL.
+     * @brief Default constructor for the MosqueDrawer class.
+     */
+    MosqueDrawer();
+
+    /**
+     * @brief Draws a customizable dome using OpenGL.
+     *
+     * This method draws a dome at the specified position with a customizable size and color.
      *
      * @param position Center position of the dome.
      * @param size Scaling factor for size.
      * @param color RGB color of the dome.
      */
     void drawDome(const Point& position, const float size, const Color& color);
+
+    /**
+     * @brief Draws the first type of prayer carpet.
+     *
+     * This method draws a prayer carpet with a specific texture at the provided vertices.
+     *
+     * @param points An array of points representing the vertices of the carpet.
+     * @param count The number of vertices in the array.
+     */
     void drawPrayerCarbet1(const Point points[4], const int count);
+
+    /**
+     * @brief Draws the second type of prayer carpet.
+     *
+     * This method draws a prayer carpet with a specific texture at the provided vertices.
+     *
+     * @param points An array of points representing the vertices of the carpet.
+     * @param count The number of vertices in the array.
+     */
     void drawPrayerCarbet2(const Point points[4], const int count);
 
 private:
-     static Model_3DS* crescentModel;  
-     int carbet1, carbet2;
-     void drawCarbet(const Point points[4], const int count, const int textureID);
+    /**
+     * @brief Static model for the crescent used in the mosque.
+     */
+    static Model_3DS* crescentModel;
+
+    /**
+     * @brief Texture ID for the first type of prayer carpet.
+     */
+    int carbet1;
+
+    /**
+     * @brief Texture ID for the second type of prayer carpet.
+     */
+    int carbet2;
+
+    /**
+     * @brief Draws a prayer carpet with a specific texture at the provided vertices.
+     *
+     * @param points An array of points representing the vertices of the carpet.
+     * @param count The number of vertices in the array.
+     * @param textureID The texture ID for the carpet.
+     */
+    void drawCarbet(const Point points[4], const int count, const int textureID);
 };
 
 #endif // MOSQUE_DRAWER_H
